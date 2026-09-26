3042dd1aab9ba57e545e49962ebe33dacc3b9752
Logan Shire <logan.shire@gmail.com>
2026-09-26 11:38:27 -0400

    agent_ui: the scroll starts after the inserts land (iOS 26 crash)

--- third_party/swift_packages/Package.swift
+++ third_party/swift_packages/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/migueldeicaza/SwiftTerm.git", exact: "1.10.1"),
         .package(
             url: "https://github.com/AttilaTheFun/agent_ui.git",
-            revision: "71e87505885c8adce088339196e439e414a0837e"
+            revision: "b4ea12126abc2c942e971e8f4bc7d6b632ab24ca"
         ),
         // The transcript cache: indexed, searchable rows of every session's
         // file, kept by the server off the main thread.
