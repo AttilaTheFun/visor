fe6f6dc39c9e2861526e956b710892f5189692dd
Logan Shire <logan.shire@gmail.com>
2026-09-26 11:29:13 -0400

    agent_ui: rows arriving come in with the scroll as one animated change

--- third_party/swift_packages/Package.swift
+++ third_party/swift_packages/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/migueldeicaza/SwiftTerm.git", exact: "1.10.1"),
         .package(
             url: "https://github.com/AttilaTheFun/agent_ui.git",
-            revision: "c63ba106acf221830c5691bcdaebe09652403a3c"
+            revision: "71e87505885c8adce088339196e439e414a0837e"
         ),
         // The transcript cache: indexed, searchable rows of every session's
         // file, kept by the server off the main thread.
